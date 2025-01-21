4e76b893e71a8f057234e16d45ab505c0fa1910a
rionkatsu <loadingforloading@gmail.com>
2025-01-21 22:23:22 +0800

    Welcom

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+// For other other information see Castiel
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hi,Mom");
+var read = Console.ReadLine();
+Console.WriteLine(" Hello " + read);
